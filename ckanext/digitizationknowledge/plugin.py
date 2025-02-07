0bd310f3105351e550dbe1e8fd49a8fcd16cd53e
Fritz J. Pichardo Marcano <fpichardom@yahoo.com>
2025-02-07 03:02:59 +0100

    Add custom facets to search interface

--- ckanext/digitizationknowledge/plugin.py
+++ ckanext/digitizationknowledge/plugin.py
@@ -1,5 +1,6 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from collections import OrderedDict
 
 
 # import ckanext.digitizationknowledge.cli as cli
@@ -20,7 +21,8 @@ class DigitizationknowledgePlugin(plugins.SingletonPlugin):
     # plugins.implements(plugins.IClick)
     # plugins.implements(plugins.ITemplateHelpers)
     plugins.implements(plugins.IValidators)
-    
+    plugins.implements(plugins.IFacets)
+    plugins.implements(plugins.IPackageController)
 
     # IConfigurer
 
@@ -60,3 +62,71 @@ class DigitizationknowledgePlugin(plugins.SingletonPlugin):
     def get_validators(self):
         return validators.get_validators()
     
+    # IFacets
+
+    def dataset_facets(self, facets_dict, package_type):
+        """Modify the facets_dict and return it
+        """
+        new_facets = OrderedDict()
+        new_facets['type'] = toolkit._('Digitization Resource Type')
+        new_facets['category'] = toolkit._('Categories')
+        #new_facets['task'] = toolkit._('Tasks')
+
+        # Add the rest of the facets
+        for key, value in facets_dict.items():
+            new_facets[key] = value
+
+        return new_facets
+    
+    def group_facets(self, facets_dict, group_type, package_type):
+        return facets_dict
+    
+    def organization_facets(self, facets_dict, organization_type, package_type):
+        return facets_dict
+
+    def before_dataset_index(self, pkg_dict):
+        # Get the task_cluster value
+
+        # for field_name in self.JSON_LIST_FIELDS:
+        #     if field_name in pkg_dict:
+        #         pkg_dict[field_name] = self._process_json_list_field(
+        #             pkg_dict.get(field_name, ''),
+        #             field_name
+        #         )
+        return pkg_dict
+   
+    # Required methods with default implementations
+    
+    def before_dataset_search(self, search_params):
+        return search_params
+
+    def after_dataset_search(self, search_results, search_params):
+        return search_results
+
+    def before_dataset_view(self, pkg_dict):
+        return pkg_dict
+
+    def after_dataset_create(self, context, pkg_dict):
+        return pkg_dict
+
+    def after_dataset_update(self, context, pkg_dict):
+        return pkg_dict
+
+    def after_dataset_delete(self, context, pkg_dict):
+        return pkg_dict
+
+    def after_dataset_show(self, context, pkg_dict):
+        return pkg_dict
+
+    def create(self, entity):
+        pass
+
+    def edit(self, entity):
+        pass
+
+    def delete(self, entity):
+        pass
+
+    def read(self, entity):
+        pass
+
