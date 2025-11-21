6fddc0ffcb678f9ec5658fbf45a1c653bfdf9858
Fritz J. Pichardo Marcano <fpichardom@yahoo.com>
2025-11-21 02:26:00 +0000

    feat: add terms of use and privacy policy pages with blueprint routes

--- ckanext/digitizationknowledge/views.py
+++ ckanext/digitizationknowledge/views.py
@@ -1,17 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 digitizationknowledge = Blueprint(
     "digitizationknowledge", __name__)
 
 
-def page():
-    return "Hello, digitizationknowledge!"
+#def page():
+#    return "Hello, digitizationknowledge!"
 
+
+#digitizationknowledge.add_url_rule(
+#    "/digitizationknowledge/page", view_func=page)
+
+
+# Define page for Terms of Use
+
+def terms_of_use():
+    "Render the Terms of use page."
+    return render_template('digitizationknowledge/terms_of_use.html')
 
 digitizationknowledge.add_url_rule(
-    "/digitizationknowledge/page", view_func=page)
+    "/terms-of-use", view_func=terms_of_use
+) 
 
+# Define page for privacy policy
+
+def privacy_policy():
+    "Render the privacy policy page."
+    return render_template('digitizationknowledge/privacy_policy.html')
+
+digitizationknowledge.add_url_rule(
+    "/privacy-policy", view_func=privacy_policy
+)
 
 def get_blueprints():
     return [digitizationknowledge]
